561775da821c7a07a26664d7e1082a62b914c9a0
Gabrielramirez2003 <gabriel113_@outlook.com>
2026-06-23 13:15:54 -0300

    Initial commit

---
+++ src/main/java/com/example/AthleteOS/AthleteOsApplication.java
@@ -0,0 +1,13 @@
+package com.example.AthleteOS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AthleteOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AthleteOsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/AthleteOS/AthleteOsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.AthleteOS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthleteOsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
